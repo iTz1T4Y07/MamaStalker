c4a99aa762b48f219f9d19b4cffa791d5e514159
iTz1T4Y <itz1t4y@gmail.com>
2022-01-20 10:50:29 +0200

    Implemented Server ctor & Start()

--- MamaStalker.Server/MamaStalker.Server/Server.cs
+++ MamaStalker.Server/MamaStalker.Server/Server.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,31 @@ namespace MamaStalker.Server
 {
     public class Server
     {
+        private readonly TcpListener _listener;
+
+        private readonly IList<TcpClient> _clients;
+
+        private readonly int _refreshInterval;
+
+        public Server(int port, int RefreshInterval)
+        {
+            _listener = new TcpListener(IPAddress.Any, port);
+            _clients = new List<TcpClient>();
+            _refreshInterval = RefreshInterval;
+        }
+
+        public void Start()
+        {
+            _listener.Start(50);
+            _listener.BeginAcceptTcpClient(new AsyncCallback(AcceptNewConnection), _listener);
+            
+        }
+
+        private void AcceptNewConnection(IAsyncResult asyncResult)
+        {
+            TcpListener listener = (TcpListener) asyncResult.AsyncState;
+            TcpClient newClient = listener.EndAcceptTcpClient(asyncResult);
+            _clients.Add(newClient);            
+        }
     }
 }
