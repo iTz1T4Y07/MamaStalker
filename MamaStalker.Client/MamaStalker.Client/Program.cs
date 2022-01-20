5b34ff3c07353812f65715166753873bffdb0755
iTz1T4Y <itz1t4y@gmail.com>
2022-01-20 12:11:15 +0200

    Implemented main()

--- MamaStalker.Client/MamaStalker.Client/Program.cs
+++ MamaStalker.Client/MamaStalker.Client/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,8 @@ namespace MamaStalker.Client
     {
         static void Main(string[] args)
         {
+            Client client = new Client();
+            client.Start(IPAddress.Loopback, 9090).GetAwaiter().GetResult();
         }
     }
 }
