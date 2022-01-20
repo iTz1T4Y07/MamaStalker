bebc5ee9c25b5fb441c4e5094ec4248861df256d
iTz1T4Y <itz1t4y@gmail.com>
2022-01-20 11:39:48 +0200

    Implemented main

--- MamaStalker.Server/MamaStalker.Server/Program.cs
+++ MamaStalker.Server/MamaStalker.Server/Program.cs
@@ -10,6 +10,30 @@ namespace MamaStalker.Server
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter required port");
+            int port;
+            if (!int.TryParse(Console.ReadLine(), out port))
+            {
+                Console.WriteLine("Invalid port. Closing program!");
+                return;
+            }
+
+            Console.WriteLine("Please enter a refresh interval in milliseconds.");
+            int refreshInterval;
+            if (!int.TryParse(Console.ReadLine(), out refreshInterval))
+            {
+                Console.WriteLine("Invalid refresh rate. Closing program!");
+                return;
+            }
+
+            if (refreshInterval <= 0)
+            {
+                Console.WriteLine("Invalid refresh rate. Closing program!");
+                return;
+            }
+
+            Server server = new Server(port, refreshInterval);
+            server.Start().GetAwaiter().GetResult();
         }
     }
 }
